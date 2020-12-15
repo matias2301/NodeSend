3b364716bc094d4835c2679e231083ea0d1dc608
Matias Bonino <matias2301@hotmail.com>
2020-12-15 15:49:49 -0300

    first commit

---
+++ back/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.listen( port, '0.0.0.0', () => {
+    console.log(`Listening port ${port}`)
+});
