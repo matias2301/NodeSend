a64fbdc70d4b5ab71b026dd76913274e2df8e5de
Matias Bonino <matias2301@hotmail.com>
2020-12-19 16:20:54 -0300

    upload folder created

--- front/next.config.js
+++ front/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        backendURL: 'http://localhost:4000',
-        frontendURL: 'http://localhost:3000'
+        backendURL: 'https://shielded-tor-37937.herokuapp.com/',
+        frontendURL: 'https://node-send.vercel.app/'
     }
 }
